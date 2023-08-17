77ae7501b2eff2dc6c54f51f25e9b41446a1c3dd
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2023-08-17 15:39:02 +0200

    added FlatDict class

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import logging
+from typing import Self, TypeVar
+_V = TypeVar('_V')
 
+############# LOGGER #############
 logging.basicConfig(level=logging.DEBUG, datefmt='%Y-%m-%dZ%z %H:%M:%S')
 
 class CustomFormatter(logging.Formatter):
@@ -22,3 +25,37 @@ logger = logging.getLogger()
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(CustomFormatter())
 logger.handlers = [console_handler]
+
+
+############# UN-FLATTENER #############
+
+class FlatDict(dict):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+  def get(self, key: str, *default_values):
+    for default_value in default_values:
+      value = super().get(key, default_value)
+      if value is not None: return value
+    return default_values[-1]
+
+  def __getitem__(self, key: str) -> str | None | Self:
+    value = self
+    for k in key.split('.'):
+      if isinstance(value, dict): value = value.get(k)
+      elif value is None: return None
+      else: raise KeyError(key)
+    return value
+
+  def __setitem__(self, key: str, value: _V) -> _V:
+    keys = key.split('.')
+    last_key = keys.pop()
+    target = self
+    for k in keys:
+      if k in target and not isinstance(target[k], dict) and k is not last_key:
+        raise ValueError('Cannot overwrite data that is not at the end of the path')
+      if k not in target: target[k] = {}
+      target = target.get(k, {})
+
+    target[last_key] = value
+    return value
