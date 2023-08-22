b9292653a8c7046bce1008f2c6d29e3faa0c9f29
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2023-08-22 15:13:09 +0200

    added randomnumber command

---
+++ commands/useful/randomnumber.py
@@ -0,0 +1,32 @@
+from random import randint
+from math import isnan
+
+from utils import Command, Aliases, Option
+
+class RandomNumber(Command):
+  name = 'randomnumber'
+  aliases = Aliases(prefix=['random-number'])
+  slashCommand = True
+  prefixCommand = True
+  dmPermission = True
+  ephemeralDefer = True
+  options = [
+      Option(name='minimum', type='Integer'),
+      Option(name='maximum', type='Integer')
+  ]
+
+  def run(self, msg, lang):
+    min_value = msg.options.getInteger('minimum') or int(msg.args[0]) if msg.args else None
+    max_value = msg.options.getInteger('maximum') or int(msg.args[1]) if len(msg.args) > 1 else None
+
+    if min_value is None or isnan(min_value): min_value = 0
+    if max_value is None or isnan(max_value): max_value = 100
+
+    try:
+      result = randint(min_value, max_value) if min_value < max_value else randint(max_value, min_value)
+      msg.customReply(str(result))
+    except ValueError as err:
+      if str(err) == 'empty range for randrange()':
+        msg.customReply(lang('outOfRange', err.args[0]))
+      else:
+        raise err
