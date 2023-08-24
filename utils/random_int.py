b7c8e0e1df9e922dfe5c70fa99657a631057fdd0
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2023-08-24 16:10:48 +0200

    fixed overwriting build-in vars

--- utils/random_int.py
+++ utils/random_int.py
@@ -2,7 +2,7 @@ from secrets import randbelow
 from sys import maxsize
 
 
-def random_int(max: int = maxsize + 1, min: int = 0):
+def random_int(max_: int = maxsize + 1, min_: int = 0):
   """`max - min +1` must be greater then 0"""
-  if min > max: min, max = max, min
-  return randbelow(max - min + 1) + min
+  if min_ > max_: min_, max_ = max_, min_
+  return randbelow(max_ - min_ + 1) + min_
